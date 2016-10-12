90cc2fc8c78894ab002078ce17380b786ecb8ff2
quanvanphu <phubknic@gmail.com>
2016-10-12 02:52:06 +0000

    Signup-Signin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(username: "quanphu",
+            email: "phu@gmail.com",
+            password: "12345678",
+            password_confirmation: "12345678")
+50.times do |n|
+	username = Faker::Name.name
+	email = "phubknic-#{n+1}@gmail.com"
+	password = "password"
+	User.create!(username:username,
+				 email:email,
+				 password:password,
+				 password_confirmation:password)
+end
